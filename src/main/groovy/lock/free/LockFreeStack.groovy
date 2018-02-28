7227c48d419c5c36b431cf843872b5b99830fcf9
Michal Kalkowski <mkalkowski@users.noreply.github.com>
2018-02-28 09:54:18 +0100

    a

--- src/main/groovy/lock/free/LockFreeStack.groovy
+++ src/main/groovy/lock/free/LockFreeStack.groovy
@@ -1,15 +1,58 @@
 package lock.free
 
+import groovy.transform.Canonical
+import groovy.util.logging.Slf4j
+
 import java.util.concurrent.atomic.AtomicReference
 
+@Slf4j
 class LockFreeStack {
 
-    AtomicReference<List<Integer>> stack = new AtomicReference<>(null)
+    AtomicReference<Node> head = new AtomicReference<>(null)
 
-    public void pop() {
-        int i = 2**2
-        int x = Math.log(i)
+    def pop() {
+        int tryCounter
+        Node result
+        while ({
+            def oldHead = head.get()
+            if (oldHead == null) return null
+            result = oldHead
+            def newHead = oldHead.next
+            !head.compareAndSet(oldHead, newHead)
+        }()) continue
+        log.info "Finished popping after ${tryCounter} tries"
+
+        result.value
+    }
+
+    def push(Integer newVal) {
+        int tryCounter
+        while ({
+            ++tryCounter
+            def old = head.get()
+            def newNode = new Node(newVal, old)
+            !head.compareAndSet(old, newNode)
+        }()) continue
+        log.info "Finished pushing ${newVal} after ${tryCounter} tries"
+    }
+
+    @Canonical
+    class Node {
+        Integer value
+        Node next
     }
 
 
+    static void main(String[] args) {
+        def stack = new LockFreeStack()
+
+        stack.push(3)
+        stack.push(2)
+        stack.push(1)
+
+        assert stack.pop() == 1
+        assert stack.pop() == 2
+        assert stack.pop() == 3
+
+    }
 }
